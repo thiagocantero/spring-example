bbb42524460829ca67e0324413134cc8232365c5
thiagocantero <thiagocantero@gmail.com>
2025-06-13 17:22:34 -0300

    ft Main

---
+++ src/main/java/br/com/thiago/spring_example/SpringExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiago.spring_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+}
