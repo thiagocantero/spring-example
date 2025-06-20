10f20f574365b4b487e95e999e57bb6eacd7b8af
thiagocantero <thiagocantero@gmail.com>
2025-06-20 14:59:16 -0300

    Thymeleaf Objects in template

--- src/main/java/br/com/thiago/spring_example/TestController.java
+++ src/main/java/br/com/thiago/spring_example/TestController.java
@@ -3,9 +3,9 @@ package br.com.thiago.spring_example;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,20 +30,20 @@ public class TestController {
         return new ModelAndView("teste", Map.of("nome", "Malu"));
     }
 
+    /**
+     * @return
+     */
     @GetMapping("/teste3")
     public String action3(){
+        Model.addAttribute("client", new Client("Thiago", 42));
         return "form";
     }
 
     @PostMapping("/teste3")
-    public String action4(@ModelAttribute("name") String nome,
-                          @ModelAttribute("age") int idade)  
-    {
-        System.out.println(nome);
-        System.out.println(idade);
-
+    public String action4(Client client) {
+        System.out.println(client);
         return "form";
     }
-    
+   
 
 }
