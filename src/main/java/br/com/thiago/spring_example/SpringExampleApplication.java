3e77553f2c1b4d52f6218f4db1729530711ca094
thiagocantero <thiagocantero@gmail.com>
2025-06-22 16:55:52 -0300

    IoC of Client Repository in entry point application

--- src/main/java/br/com/thiago/spring_example/SpringExampleApplication.java
+++ src/main/java/br/com/thiago/spring_example/SpringExampleApplication.java
@@ -1,13 +1,28 @@
 package br.com.thiago.spring_example;
 
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringExampleApplication {
+public class SpringExampleApplication implements CommandLineRunner {
+
+	
+	private ClientRepository clientRepository;
+
+	public SpringExampleApplication(ClientRepository clientRepository) {
+		this.clientRepository = clientRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(clientRepository);
+		System.out.println("A aplicação iniciou");
 	}
 
 }
