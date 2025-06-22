4ab599bc72f19ba4bb88e8fc38ad2dff704b4369
thiagocantero <thiagocantero@gmail.com>
2025-06-22 15:56:52 -0300

    Ft RequestParam and PathVariable Notations

--- src/main/java/br/com/thiago/spring_example/TestController.java
+++ src/main/java/br/com/thiago/spring_example/TestController.java
@@ -1,12 +1,17 @@
 package br.com.thiago.spring_example;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 
 
@@ -34,16 +39,35 @@ public class TestController {
      * @return
      */
     @GetMapping("/teste3")
-    public String action3(ModelMap model2){
-        model2.addAttribute("Client", new Client("Thiago", 42));
+    public String action3(Model model){
+        model.addAttribute("Client", new Client("Thiago", 42));
         return "form";
     }
 
     @PostMapping("/teste3")
-    public String action4(Client client) {
+    public String action4(@ModelAttribute("client") Client client) {
         System.out.println(client);
         return "form";
     }
    
+    @GetMapping("/teste5")
+    public String action5(@RequestParam(name ="q", required = false, defaultValue = "Malu") 
+    String busca,
+    @RequestParam int age,
+    @RequestParam List<String> skills
+    ){
+        System.out.println(busca);
+        System.out.println(age);
+        System.out.println(skills);
+        return "teste";
+    }
 
-}
+    @GetMapping("/teste6/{id}")
+    public String action6(@PathVariable Long id){
+        System.out.println(id);
+        return "teste";
+        }
+    }
+    
+
+
